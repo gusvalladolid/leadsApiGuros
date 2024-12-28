cf51ddedde7816a121720d04d715dbd975a4e070
gusvalladolid <gusvalladolidtt@gmail.com>
2024-12-28 16:38:40 -0600

    Changed database connection

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -6,14 +6,18 @@ dotenv.config()
 class Database {
   public sequelize: Sequelize | undefined
 
+  /*
   private readonly POSTGRES_DB = process.env.POSTGRES_DB as string
   private readonly POSTGRES_USER = process.env.POSTGRES_USER as string
   private readonly POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD as string
   private readonly POSTGRES_HOST = process.env.POSTGRES_HOST as string
   private readonly POSTGRES_PORT = process.env.POSTGRES_PORT as unknown as number
+*/
+  protected POSTGRES_DB_LINK = process.env.POSTGRES_DB_LINK as string
 
   constructor () {
     this.connectToPostgres().then(() => {
+      console.log(this.POSTGRES_DB_LINK)
       console.log('Database connection established.')
     }).catch((err: unknown) => {
       console.error('Error connecting to the database:', err)
@@ -21,20 +25,13 @@ class Database {
   }
 
   private async connectToPostgres (): Promise<void> {
-    this.sequelize = new Sequelize({
+    this.sequelize = new Sequelize(this.POSTGRES_DB_LINK, {
       dialect: 'postgres',
-      host: this.POSTGRES_HOST,
-      port: this.POSTGRES_PORT,
-      username: this.POSTGRES_USER,
-      password: this.POSTGRES_PASSWORD,
-      database: this.POSTGRES_DB,
-      models: [Lead],
+      protocol: 'postgres',
       dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
+        ssl: true
+      },
+      models: [Lead]
     })
     await this.sequelize.authenticate()
   }
